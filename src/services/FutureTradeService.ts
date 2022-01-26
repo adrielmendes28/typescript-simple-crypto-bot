4181972a64bcdc5685e51a925aced3cac45312e0
Adriel Mendes <adriel.amendes12@gmail.com>
2022-01-26 00:06:58 -0300

    alteracoes

--- src/services/FutureTradeService.ts
+++ src/services/FutureTradeService.ts
@@ -4,30 +4,30 @@ export class FutureTradeService {
   private initialAmountUSDT = 2;
   private running = false;
   private users = [
-    {
-      name: "Adriel",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "gkrHT41fWQbBEhbbd3N6xPtVcJWXPUjppaG7NXPvkSjl68BqM8YOcszR5SndQs1q",
-        APISECRET:
-          "NZGcjjmJLA1jMtPe4may9VHRCMIBvDfuJ8rssXaqDnVPvmerxKfpTyLqTX08pK3s",
-      }),
-    },
-    {
-      name: "Juliana",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "q5oz7Dta5FMZlwkZxbum08xFX5s2oa4qAwWydLDWW3FJnCQ5DOO4XnGqdmH106kP",
-        APISECRET:
-          "9p4jFrl8K6hbAc5PEecQSUmB6OOb7Uu04vDZpb6Eh3m84L92oswnPvDl59VGGjOx",
-      }),
-    },
+    // {
+    //   name: "Adriel",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "gkrHT41fWQbBEhbbd3N6xPtVcJWXPUjppaG7NXPvkSjl68BqM8YOcszR5SndQs1q",
+    //     APISECRET:
+    //       "NZGcjjmJLA1jMtPe4may9VHRCMIBvDfuJ8rssXaqDnVPvmerxKfpTyLqTX08pK3s",
+    //   }),
+    // },
+    // {
+    //   name: "Juliana",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "q5oz7Dta5FMZlwkZxbum08xFX5s2oa4qAwWydLDWW3FJnCQ5DOO4XnGqdmH106kP",
+    //     APISECRET:
+    //       "9p4jFrl8K6hbAc5PEecQSUmB6OOb7Uu04vDZpb6Eh3m84L92oswnPvDl59VGGjOx",
+    //   }),
+    // },
     {
       name: "Thiago",
       maxROIGain: 2,
@@ -40,66 +40,66 @@ export class FutureTradeService {
           "WPIUb88Zy3Jge46jlUGsl8oUaB1WEP5zUZe4udD5rSXA0obsAu7tfOUqTCuxRw2q",
       }),
     },
-    {
-      name: "Bruna",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "ZwC1ny9cRKZXvazqD4MXxtCshQ0ulppOsrcoGEBQ0lACQPgNhAWJdxHHDwxD8XiP",
-        APISECRET:
-          "NYXIxtaGBOVX8gQfJvpJH1ATMSVI3SHDCWPBzqljPe4VCzg87Xug63WrSgDc1nnJ",
-      }),
-    },
-    {
-      name: "Juliana2",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "rHSKrv2MQKNft91dLLwHMcQR7sn731kaEyqZKUvNx1uF4ktsgvSuacVQe9cq8999",
-        APISECRET:
-          "tkdH70W1OUNkEb74rebJ0sHuJbTbqur5zeOhPefsKOv7bjHvINGBuBzWd9MIROpA",
-      }),
-    },
-    {
-      name: "Alessandra",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "DiM9jbp8GeyFdxnT5S557IIiYJuMvaQh4mIOhmXINJjE8pfwmkgqDPCSxTRPvOCD",
-        APISECRET:
-          "DoBuJH6CAym77tcfEJIr07CXTlge3PUyaphENPUuYIyWjqhSIADP7s1jz5MicwiC",
-      }),
-    },
-    {
-      name: "Simone",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "Q3GzAsOqBSa6PakO1h1TjifTfdsB4xpnxmEFL0Mn4ogUuMkCtQLre1nz02t5M9aA",
-        APISECRET:
-          "xbs1ZxooZIPOjwRf77o3w7CQfut2AlpVsX9sCTZwRh6PAMnvWVaLTInZSV01TruH",
-      }),
-    },
-    {
-      name: "Edlaine",
-      maxROIGain: 2,
-      maxROILoss: -5,
-      reducePercentage: 1,
-      clientApi: new Binance().options({
-        APIKEY:
-          "kaCgWizO2p332srnLjfGDF3fat89tFaMuH16ELf4wL80MokNfbw4ArJJXN1dgVMb",
-        APISECRET:
-          "ZwWVSDEsMAvUPtciTZU4Atuf7shJllxWajgpcT4FbpwphlBewqhRu3n9hTDYy9eb",
-      }),
-    },
+    // {
+    //   name: "Bruna",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "ZwC1ny9cRKZXvazqD4MXxtCshQ0ulppOsrcoGEBQ0lACQPgNhAWJdxHHDwxD8XiP",
+    //     APISECRET:
+    //       "NYXIxtaGBOVX8gQfJvpJH1ATMSVI3SHDCWPBzqljPe4VCzg87Xug63WrSgDc1nnJ",
+    //   }),
+    // },
+    // {
+    //   name: "Juliana2",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "rHSKrv2MQKNft91dLLwHMcQR7sn731kaEyqZKUvNx1uF4ktsgvSuacVQe9cq8999",
+    //     APISECRET:
+    //       "tkdH70W1OUNkEb74rebJ0sHuJbTbqur5zeOhPefsKOv7bjHvINGBuBzWd9MIROpA",
+    //   }),
+    // },
+    // {
+    //   name: "Alessandra",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "DiM9jbp8GeyFdxnT5S557IIiYJuMvaQh4mIOhmXINJjE8pfwmkgqDPCSxTRPvOCD",
+    //     APISECRET:
+    //       "DoBuJH6CAym77tcfEJIr07CXTlge3PUyaphENPUuYIyWjqhSIADP7s1jz5MicwiC",
+    //   }),
+    // },
+    // {
+    //   name: "Simone",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "Q3GzAsOqBSa6PakO1h1TjifTfdsB4xpnxmEFL0Mn4ogUuMkCtQLre1nz02t5M9aA",
+    //     APISECRET:
+    //       "xbs1ZxooZIPOjwRf77o3w7CQfut2AlpVsX9sCTZwRh6PAMnvWVaLTInZSV01TruH",
+    //   }),
+    // },
+    // {
+    //   name: "Edlaine",
+    //   maxROIGain: 2,
+    //   maxROILoss: -5,
+    //   reducePercentage: 1,
+    //   clientApi: new Binance().options({
+    //     APIKEY:
+    //       "kaCgWizO2p332srnLjfGDF3fat89tFaMuH16ELf4wL80MokNfbw4ArJJXN1dgVMb",
+    //     APISECRET:
+    //       "ZwWVSDEsMAvUPtciTZU4Atuf7shJllxWajgpcT4FbpwphlBewqhRu3n9hTDYy9eb",
+    //   }),
+    // },
   ];
 
   delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -159,7 +159,7 @@ export class FutureTradeService {
         const differenceROI = maxROIReached - inputROI;
         //Comparar se o ROI salvo na operação é maior que o atual
         if (inputROI > maxROIReached) {
-          if (inputROI > 0) {
+          if (inputROI > 1) {
             await Order.findOneAndUpdate(
               { cliente, symbol, status: 'open' },
               {
